37bc7548aa41326e96668a6bd7f9578e38f483a2
Alex Mirniy <alex_mirn@ukr.net>
2015-07-15 19:02:44 +0300

    one more time did oneProject in spring_mvc

---
+++ src/main/java/com/go_java4/alex_mirn/controller/OneProjectController.java
@@ -0,0 +1,27 @@
+package com.go_java4.alex_mirn.controller;
+
+import com.go_java4.alex_mirn.model.entity.Project;
+import com.go_java4.alex_mirn.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("/oneProject")
+public class OneProjectController {
+    @Autowired
+    ProjectService projectService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String showOneProject(ModelMap model, @RequestParam String project) {
+        int intProject = Integer.valueOf(project);
+        Project oneProject = projectService.getById(intProject);
+        model.addAttribute("oneProject", oneProject);
+        return "oneProject";
+    }
+}
